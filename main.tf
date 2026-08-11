3189acf2ac73c7f497072cadf19ecb3eb44a8d98
tandzmtr08 <tetianazagurska036@gmail.com>
2026-08-11 03:21:08 +0000

    Add 100 pod resource quota

--- main.tf
+++ main.tf
@@ -6,6 +6,20 @@ resource "kubernetes_namespace_v1" "this" {
   }
 }
 
+resource "kubernetes_resource_quota_v1" "pod_limit" {
+  metadata {
+    name      = "${kubernetes_namespace_v1.this.metadata[0].name}-pod-limit"
+    namespace = kubernetes_namespace_v1.this.metadata[0].name
+  }
+
+  spec {
+    hard = {
+      pods = "100"
+    }
+  }
+}
+
+
 
 
 
